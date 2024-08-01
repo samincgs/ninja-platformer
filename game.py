ef17d177877803ee1f9a86157c6c00a431ed708b
samin <samincgs@gmail.com>
2024-08-01 16:30:27 -0400

    created a camera that follows around the player with some delay and also added a parallax clouds system

--- game.py
+++ game.py
@@ -2,41 +2,64 @@ import pygame, sys
 from scripts.entities import PhysicsEntity
 from scripts.utils import load_image, load_images
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
 class Game:
     def __init__(self):
         pygame.init()
 
+        # main screen
         self.screen = pygame.display.set_mode((640, 480))
+        
+        # display screen made to enlarge the pixels of the entities/tiles
         self.display = pygame.Surface((320, 240))
         
         pygame.display.set_caption('ninja platformer')
 
         self.clock = pygame.time.Clock()
         
+        # assets for all the imports
         self.assets = {
             'player': load_image('entities/player.png'),
             'grass': load_images('tiles/grass'),
             'stone': load_images('tiles/stone'),
             'decor': load_images('tiles/decor'),
             'large_decor': load_images('tiles/large_decor'),
+            'background': load_image('background.png'),
+            'clouds' : load_images('clouds')
         }
         
-        self.tilemap = Tilemap(self, tile_size=16)
-        
+        self.clouds = Clouds(self.assets['clouds'], count=16)
         
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
         self.movement = [False, False]
+        
+        # tilemap
+        self.tilemap = Tilemap(self, tile_size=16)
+        
+        # camera
+        self.scroll = [0, 0]
+        
+        
 
     def run(self):
-        while True:
+        while True: 
             
-            self.display.fill((14, 219, 248))
+            self.display.blit(self.assets['background'], (0, 0))
             
-            self.tilemap.render(self.display)
+            # camera
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+            
+            # clouds
+            self.clouds.update()
+            self.clouds.render(self.display,offset= render_scroll)
+            
+            self.tilemap.render(self.display, offset=render_scroll)
             
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display)
+            self.player.render(self.display, offset=render_scroll)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
