1f93dea37c6729394e707d29e3fdca7c22191db9
samin <samincgs@gmail.com>
2024-08-12 17:29:55 -0400

    added clouds with varying depths to simulate a parallax effect

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import sys
 from scripts.entities import PhysicsEntity
 from scripts.utils import load_image, load_images
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
 class Game:
     def __init__(self):
@@ -24,10 +25,14 @@ class Game:
             'decor': load_images('tiles/decor'),
             'large_decor': load_images('tiles/large_decor'),
             'player': load_image('entities/player.png'),
+            'clouds': load_images('clouds')
             
         }
         
         self.movement = [False, False]
+        
+        self.clouds = Clouds(self.assets['clouds'], count= 16)
+        
         self.player = PhysicsEntity(self, 'player', (50, 50), self.assets['player'].get_size())
         
         self.tilemap = Tilemap(self, tile_size=16)
@@ -44,6 +49,9 @@ class Game:
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll)
+            
             self.tilemap.render(self.display, offset=render_scroll)
             
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))  
